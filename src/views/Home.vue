a14e7c102022e24a1b1024b23e641439de870b60
Mohab Abugabal <mabugabal@sorint.it>
2021-11-15 13:08:06 +0100

    practing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="home">
-   <h1>Watch and Watch Effect</h1>
+  <div class="home">  
+<h1>Watch </h1>
 
-   
+<div v-for="name in matchingNames" :key="name"> {{name}}</div>
+
+<input type="text" v-model="search">
+
+<p>Search term: {{search}}</p>
+
+
   </div>
 </template>
 
 <script>
+import {ref, computed } from '@vue/reactivity'
+import { watch, watchEffect } from '@vue/runtime-core'
 
 export default {
   name: 'Home',
-  components: {}
+  setup(){
+
+   const names = ref(['Saka', 'SmithRow', 'Aoba', 'Odegard', 'Nenny'])
+
+   const search = ref("")
+
+   watch(search, () => {
+     console.log("watch fn ran");
+     console.log(search.value);
+   });
+
+   watchEffect(() => {
+     console.log("watch effect fn run", search.value)
+   })
+
+   const matchingNames = computed(() => {
+
+     return names.value.filter((name)=> name.includes(search.value) )
+
+
+   })
+
+    return {names, search, matchingNames}
+
+
+
+  },
 }
 </script>
