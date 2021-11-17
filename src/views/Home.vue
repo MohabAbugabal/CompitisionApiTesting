abfba3d1ed6e4aa28f0927d545e62cd5b72ded72
Mohab Abugabal <mabugabal@sorint.it>
2021-11-17 11:37:54 +0100

    practicing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,37 @@
   <div class="home">  
 <h1>Props </h1>
 
+<PostList :posts = "posts" />
   </div>
 </template>
 
 <script>
+import PostList from '../components/PostList.vue'
+import {ref} from 'vue';
 
 export default {
   name: 'Home',
+  components: {
+    PostList
+    }
+  ,
   setup(){
 
+    const posts = ref([{
+      title: 'welcome the blog',
+      body: 'Hello world 1',
+      id: 1
+    }, 
+    {
+    title: 'top tips',
+    body: 'Hello world 2',
+    id:2
+    }
+    
+    ])
 
-    return {}
+
+    return {posts}
 
 
 
